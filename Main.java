33bb1bb2eb4a5f9679992ed230be5c0a3e35e233
Aaniyah <aaniyah.ellis2027@gbwl.org>
2025-09-12 12:56:09 +0000

    1.2 notes

--- Main.java
+++ Main.java
@@ -5,6 +5,19 @@ public class Main {
    public static void main(String []args) {
       System.out.println("Welcome to Java!");
       System.out.println("hiii");
+      
+      // 1. DECLARE a new varible
+      int score; 
+      // 2. ASSIGN a value to the variable
+      score = 23;
+      // 3. INITIALIZE combines declaring + assigning
+      String pokemon = "Pikachu";
+      double healthPercent = 0.75;
+      boolean isAlive = true;
 
+      // Use the plus sign th CONCATENATE (combine) Strings
+      String greeting = "Hello " + "World";
+      System.out.println(greeting);
+      // Quotation marks contain STRING LITERALS 
    }
 }
